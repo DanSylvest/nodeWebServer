e44abb522fdcc74130b4ecb46de4174f5ca92c82
alexey <rolahd@yandex.ru>
2019-02-09 19:08:08 +0300

    fix

--- webserver.js
+++ webserver.js
@@ -143,12 +143,15 @@ var path_info = function (_path) {
         is_file: false
     };
 
-    var arr = _path.split("/");
+    var stats = fs.statSync(WEB_FOLDER.replace(/\\/gm, "/") + _path.substring(1, _path.length));
+    var is_file = !stats.isDirectory();
 
-    var end = arr[arr.length - 1];
-    if(end != ""){
+    if(is_file){
+        var arr = _path.split("/");
+        var end = arr[arr.length - 1];
         info.is_file = true;
         info.file = end;
     }
+
     return info;
 };
